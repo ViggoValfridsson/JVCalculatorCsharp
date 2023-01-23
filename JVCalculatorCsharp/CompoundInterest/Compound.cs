d5e1c23bccb29ef9f059eb1031cb317d1faba0df
Jacob Erlandsson <erlandssonjacob@hotmail.com>
2023-01-23 14:17:53 +0100

    created test and functions for calculating compound interest

---
+++ JVCalculatorCsharp/CompoundInterest/Compound.cs
@@ -0,0 +1,12 @@
+﻿namespace JVCalculatorCsharp.CompoundInterest;
+
+public class Compound
+{
+    public static double CalcCompound(double startingCapital, double rateOfReturn, double years) {
+        var rateOfReturnPercent = (rateOfReturn / 100) + 1;
+
+        var totavkast = Math.Pow(rateOfReturnPercent, years);
+
+        return totavkast * startingCapital;
+    }
+}
---
+++ JVCalculatorCsharpTest/CompoundTest.cs
@@ -0,0 +1,21 @@
+﻿using JVCalculatorCsharp.CompoundInterest;
+
+namespace JVCalculatorCsharpTest;
+
+public class CompoundTest
+{
+    [Theory]
+    [InlineData(10, 10, 1, 11)]
+    [InlineData(10, 20, 1, 12)]
+
+
+    public void CompoundingTest(double startingCapital, double rateOfReturn, double years, double calculatedValue)
+    {
+        var expected = calculatedValue;
+        var result = Compound.CalcCompound(startingCapital, rateOfReturn, years);
+
+        Assert.Equal(expected, result);
+    }
+    
+}
+
