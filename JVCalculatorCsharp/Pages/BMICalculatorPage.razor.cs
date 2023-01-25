cb0a367362b18405a915f5a116048cc9d394f202
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-01-25 13:42:50 +0100

    Added full functionality for calculating bmi on BMI-page

--- JVCalculatorCsharp/Pages/BMICalculatorPage.razor.cs
+++ JVCalculatorCsharp/Pages/BMICalculatorPage.razor.cs
@@ -10,21 +10,50 @@ public partial class BMICalculatorPage
     public string? HeightFeet { get; set; }
     public string? HeightInches { get; set; }
     public string? WeightInLbs { get; set; }
+    public bool InvalidInput { get; set; } = false;
+    public bool NegativeInput { get; set; } = false;
 
-    public void HandleMetricSubmit()
+    public void HandleSubmit()
     {
         try
         {
-           
+            decimal result;
+            if(UsingMetricUnits)
+            {
+                double heightInCm = Double.Parse(HeightInCm!);
+                decimal weightInKg = Decimal.Parse(WeightInKg!);
+                if (heightInCm <= 0 || weightInKg <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                result = BMICalculator.CalculateBMI(heightInCm, weightInKg);
+            }
+            else
+            {
+                decimal heightInFeet = decimal.Parse(HeightFeet!);
+                decimal heightInInches = decimal.Parse(HeightInches!);
+                decimal weightInLbs = decimal.Parse(WeightInLbs!);
+                if (heightInFeet <= 0 || heightInInches < 0 || weightInLbs <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                result = BMICalculator.CalculateBMI(heightInFeet, heightInInches, weightInLbs);
+            }
+
+            NegativeInput = false;
+            InvalidInput = false;
+
+            BMI = $"BMI = {result}";
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            NegativeInput = true;
+            BMI = string.Empty;
         }
         catch
         {
-
+            InvalidInput = true;
+            BMI = string.Empty;
         }
     }
-    public void HandleImperialSubmit()
-    {
-
-    }
-
 }
