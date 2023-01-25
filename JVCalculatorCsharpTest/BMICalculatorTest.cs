09ddc8dcda0a6bd443a716c61c47277e732f491d
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-01-25 11:23:03 +0100

    Added test file that I wasn't tracked

---
+++ JVCalculatorCsharpTest/BMICalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using JVCalculatorCsharp.BMICalculator;
+using System;
+
+namespace JVCalculatorCsharpTest
+{
+    public class BMICalculatorTest
+    {
+        [Theory]
+        [InlineData(184, 76, 22.4)]
+        [InlineData(190, 85, 23.5)]
+        public void BMIMetricTest(double heightInCm, decimal weightInKg, decimal bmi)
+        {
+            decimal result = BMIConverter.CalculateBMI(heightInCm, weightInKg);
+            decimal expected = bmi;
+
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData(6, 1, 168, 22.2)]
+        [InlineData(6, 5, 168, 19.9)]
+        public void ImperialTest(decimal feet, decimal inches, decimal weightInLbs, decimal bmi)
+        {
+            decimal result = BMIConverter.CalculateBMI(feet, inches, weightInLbs);
+            decimal expected = bmi;
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
