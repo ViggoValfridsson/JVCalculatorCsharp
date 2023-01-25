4cfbfe17de8227279f726c46b26bbbaf61f4aea6
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-01-25 10:58:25 +0100

    Changed data types to fix precision errors in test

--- JVCalculatorCsharp/BMICalculator/BMIConverter.cs
+++ JVCalculatorCsharp/BMICalculator/BMIConverter.cs
@@ -1,4 +1,14 @@
 ﻿namespace JVCalculatorCsharp.BMICalculator;
 public class BMIConverter
 {
+    public static decimal CalculateBMI(double heightInCm, decimal weightInKg)
+    {
+        double heightSquared = Math.Pow(heightInCm, 2);
+        decimal BMI = (weightInKg / heightSquared) * 10000;
+        return BMI;
+    }
+    public static double CalculateBMI(decimal feet, decimal inches, decimal weightInLbs)
+    {
+        return 0;
+    }
 }
