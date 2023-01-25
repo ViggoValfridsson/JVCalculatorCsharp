b7c28dc1860917e39c2b2bab2927c57f131a9eac
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-01-25 11:21:59 +0100

    Added imperial bmi calculation

--- JVCalculatorCsharp/BMICalculator/BMIConverter.cs
+++ JVCalculatorCsharp/BMICalculator/BMIConverter.cs
@@ -1,14 +1,27 @@
-﻿namespace JVCalculatorCsharp.BMICalculator;
+﻿using JVCalculatorCsharp.LengthUnitCalculator;
+namespace JVCalculatorCsharp.BMICalculator;
 public class BMIConverter
 {
     public static decimal CalculateBMI(double heightInCm, decimal weightInKg)
     {
         double heightSquared = Math.Pow(heightInCm, 2);
-        decimal BMI = (weightInKg / heightSquared) * 10000;
+        decimal BMI = (weightInKg / Convert.ToDecimal(heightSquared)) * 10000;
+        return Math.Round(BMI,1);
+    }
+    public static decimal CalculateBMI(decimal feet, decimal inches, decimal weightInLbs)
+    {
+        const decimal LbsToKgRatio = 0.45359237m;
+        decimal heightInCm = ConvertFeetAndInchToCm(feet, inches);
+        decimal weightInKg = weightInLbs * LbsToKgRatio;
+
+        decimal BMI = CalculateBMI(Convert.ToDouble(heightInCm), weightInKg);
         return BMI;
     }
-    public static double CalculateBMI(decimal feet, decimal inches, decimal weightInLbs)
+    public static decimal ConvertFeetAndInchToCm(decimal feet, decimal inches)
     {
-        return 0;
+        decimal feetInCm = LengthUnitConverter.ConvertLengthUnit(feet, "Foot", "Centimeter");
+        decimal inchesInCm = LengthUnitConverter.ConvertLengthUnit(inches, "Inch", "Centimeter");
+
+        return feetInCm + inchesInCm;
     }
 }
