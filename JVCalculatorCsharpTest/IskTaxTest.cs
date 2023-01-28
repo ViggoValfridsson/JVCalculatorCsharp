7f1e1ee0447a25cab39e4ee10f39afc746f310c3
Jacob Erlandsson <erlandssonjacob@hotmail.com>
2023-01-28 11:19:24 +0100

    added UI for ISKTax

--- JVCalculatorCsharpTest/IskTaxTest.cs
+++ JVCalculatorCsharpTest/IskTaxTest.cs
@@ -6,6 +6,9 @@ namespace JVCalculatorCsharpTest
     {
         [Theory]
         [InlineData(100000, 100000, 100000, 100000, 0, 882)]
+        [InlineData(200000, 150000, 250000, 400000, 50000, 2315.25)]
+        [InlineData(400000, 150000, 175000, 300000, 100000, 2480.625)]
+
 
         public void ISKTaxCalculatorTest(decimal Q1, decimal Q2, decimal Q3, decimal Q4, decimal deposits, decimal capitalTaxToPay)
         {
