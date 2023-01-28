752fd0136b0ef38c03fd59e7db14ea9eb6c0c6c2
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-01-28 10:02:59 +0100

    Wrote tests for binary conversion

--- JVCalculatorCsharp/NumberConversion/NumberConverter.cs
+++ JVCalculatorCsharp/NumberConversion/NumberConverter.cs
@@ -1,8 +1,52 @@
-﻿namespace JVCalculatorCsharp.NumberConversion;
+﻿using Newtonsoft.Json.Converters;
+
+namespace JVCalculatorCsharp.NumberConversion;
 
 public class NumberConverter
 {
     public static string DecimalHexConverter(string input, string startUnit, string conversionUnit)
     {
+        if (startUnit == "Decimal")
+        {
+            try
+            {
+                int decimalNumber = Convert.ToInt32(input);
+
+                if (conversionUnit == "Hexadecimal")
+                {
+                    return decimalNumber.ToString("X");
+                }
+                else
+                {
+                    return input;
+                }
+            }
+            catch
+            {
+                throw new ArgumentException("Could not convert number, please enter a whole number without any letters");
+            }
+        }
+
+        try
+        {
+            int hexAsInteger = int.Parse(input, System.Globalization.NumberStyles.HexNumber);
+
+            if (conversionUnit == "Decimal")
+            {
+                return hexAsInteger.ToString();
+            }
+            else
+            {
+                return input;
+            }
+        }
+        catch
+        {
+            throw new ArgumentException("Please enter a valid hexadecimal number");
+        }
+    }
+    public static string BinaryConverter(string input, string startUnit, string conversionUnit)
+    {
+        return string.Empty;
     }
 }
--- JVCalculatorCsharpTest/NumberConversionTest.cs
+++ JVCalculatorCsharpTest/NumberConversionTest.cs
@@ -4,16 +4,31 @@ namespace JVCalculatorCsharpTest;
 public class NumberConversionTest
 {
     [Theory]
-    [InlineData("1000", "3E8", "Hexadecimal", "Decimal")]
-    [InlineData("16", "10", "Hexadecimal", "Decimal")]
-    [InlineData("15", "F", "Hexadecimal", "Decimal")]
+    [InlineData("1000", "3E8", "Decimal", "Hexadecimal")]
+    [InlineData("16", "10", "Decimal", "Hexadecimal")]
+    [InlineData("15", "F", "Decimal", "Hexadecimal")]
     [InlineData("F", "15",  "Hexadecimal", "Decimal")]
     [InlineData("3E8", "1000", "Hexadecimal", "Decimal")]
-
-    public void DecimalToHex(string input, string hex, string startUnit, string conversionUnit)
+    [InlineData("3E8", "3E8", "Hexadecimal", "Hexadecimal")]
+    [InlineData("1000", "1000", "Decimal", "Decimal")]
+    public void DecimalHexConverterTest(string input, string convertedString, string startUnit, string conversionUnit)
     {
         string result = NumberConverter.DecimalHexConverter(input, startUnit, conversionUnit);
-        var expected = hex;
+        var expected = convertedString;
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1F", "00011111", "Hexadecimal", "Binary")]
+    [InlineData("00011111", "1F", "Binary", "Hexadecimal")]
+    [InlineData("10", "1010", "Decimal", "Binary")]
+    [InlineData("1010", "10", "Binary", "Decimal")]
+    [InlineData("1010", "1010", "Binary", "Binary")]
+    public void BinaryConverter(string input, string convertedString, string startUnit, string conversionUnit)
+    {
+        string result = NumberConverter.DecimalHexConverter(input, startUnit, conversionUnit);
+        var expected = convertedString;
 
         Assert.Equal(expected, result);
     }
