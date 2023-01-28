23da1af65c66faed222e3344b931abfe1ddea6f8
Jacob Erlandsson <erlandssonjacob@hotmail.com>
2023-01-28 10:00:26 +0100

    made IskTaxConverter

---
+++ JVCalculatorCsharp/ISKTaxCalculator/ISKTaxCalculate.cs
@@ -0,0 +1,16 @@
+﻿namespace JVCalculatorCsharp.ISKTaxCalculator;
+
+
+
+public class ISKTaxCalculate
+{
+    public static decimal ISKTaxCalc(decimal Q1, decimal Q2, decimal Q3, decimal Q4, decimal deposits)
+    {
+        var avgAccountValue = (Q1 + Q2 + Q3 + Q4 + deposits) / 4;
+        var SLRplusOne = 0.0294m;
+        var standardIncome = avgAccountValue * SLRplusOne;
+        var capitalTax = 0.3m;
+        var capitalTaxToPay = standardIncome * capitalTax;
+        return capitalTaxToPay;
+    }
+}
---
+++ JVCalculatorCsharpTest/IskTaxTest.cs
@@ -0,0 +1,18 @@
+﻿using JVCalculatorCsharp.ISKTaxCalculator;
+
+namespace JVCalculatorCsharpTest
+{
+    public class IskTaxTest
+    {
+        [Theory]
+        [InlineData(100000, 100000, 100000, 100000, 0, 882)]
+
+        public void ISKTaxCalculatorTest(decimal Q1, decimal Q2, decimal Q3, decimal Q4, decimal deposits, decimal capitalTaxToPay)
+        {
+            var result = ISKTaxCalculate.ISKTaxCalc(Q1, Q2, Q3, Q4, deposits);
+            var expected = capitalTaxToPay;
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
