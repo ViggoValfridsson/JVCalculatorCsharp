de4149f649b38f9bae411754f20a8b13cdf07437
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-01-29 09:54:19 +0100

    Made length unit model fields readonly

--- JVCalculatorCsharp/Models/LengthUnitModel.cs
+++ JVCalculatorCsharp/Models/LengthUnitModel.cs
@@ -1,12 +1,12 @@
 ﻿namespace JVCalculatorCsharp.Models;
 public class LengthUnitModel
 {
-    public decimal Millimeter = 0.001m;
-    public decimal Centimeter = 0.01m;
-    public decimal Meter = 1m;
-    public decimal Kilometer = 1000m;
-    public decimal Inch = 0.0254m;
-    public decimal Foot = 0.3048m;
-    public decimal Yard = 0.9144m;
-    public decimal Mile = 1609.34m;
+    public readonly decimal Millimeter = 0.001m;
+    public readonly decimal Centimeter = 0.01m;
+    public readonly decimal Meter = 1m;
+    public readonly decimal Kilometer = 1000m;
+    public readonly decimal Inch = 0.0254m;
+    public readonly decimal Foot = 0.3048m;
+    public readonly decimal Yard = 0.9144m;
+    public readonly decimal Mile = 1609.34m;
 }
